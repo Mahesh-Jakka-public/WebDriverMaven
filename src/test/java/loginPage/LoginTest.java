10eb8cc8c5df401d49c040afac47d2a86bf47658
Bhaskar Raju <bhaskar.mahalepati@gmail.com>
2020-05-18 19:55:44 +0530

    testng

--- src/test/java/loginPage/LoginTest.java
+++ src/test/java/loginPage/LoginTest.java
@@ -1,13 +1,39 @@
 package loginPage;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class LoginTest {
- 
- public static void main(String[] args) {
- String exePath = "F:\\chromedriver.exe";
- System.setProperty("webdriver.chrome.driver", exePath);
- WebDriver driver = new ChromeDriver();
- driver.get("http://toolsqa.com/automation-practice-form/");
- }
+	
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp() {
+	System.setProperty("webdriver.chrome.driver", "F:\\SeleniumJars\\chromedriver.exe");
+	driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin() throws InterruptedException {
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
+		Thread.sleep(1000);
+		driver.findElement(By.id("identifierNext")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.name("password")).sendKeys("sdss");
+		Thread.sleep(1000);
+		driver.findElement(By.id("passwordNext")).click();
+		
+	}
+	
+	@AfterTest
+	public void tearDown() {
+	driver.quit();
+	}
+
 }
